1b0122001312ef9ec829b9c394d4364d238f908d
fetch150zy <mars_zhewei@outlook.com>
2024-11-08 02:06:20 +0800

    hw02

---
+++ hw/hw02/hw02.py
@@ -0,0 +1,239 @@
+""" Homework 2: Higher Order Functions"""
+
+HW_SOURCE_FILE = 'hw02.py'
+
+from operator import add, mul, sub
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+#####################
+# Required Problems #
+#####################
+def compose(h, g):
+    """Return a function f, such that f(x) = h(g(x)).
+    
+    >>> compose(square, triple)(5)
+    225
+    >>> double_inc = compose(increment, increment)
+    >>> double_inc(3)
+    5
+    >>> double_inc(4)
+    6
+    """
+    return lambda x: h(g(x))
+
+def product(n, f):
+    """Return the product of the first n terms in a sequence.
+    n -- a positive integer
+    f -- a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    res = 1
+    for i in range(1, n+1):
+        res *= f(i)
+    return res
+
+def accumulate(combiner, base, n, f):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are f(1), f(2), ..., f(n).  combiner is a
+    two-argument commutative, associative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
+    16
+    """
+    res = base
+    for i in range(1, n+1):
+        res = combiner(res, f(i))
+    return res
+
+def summation_using_accumulate(n, f):
+    """Returns the sum of f(1) + ... + f(n). The implementation
+    uses accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    return accumulate(add, 0, n, f)
+
+def product_using_accumulate(n, f):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'product_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    return accumulate(mul, 1, n, f)
+
+def make_repeater(h, n):
+    """Return the function that computes the nth application of h.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times! 
+    5
+    """
+    def inner(x):
+        expr = 'x'
+        for i in range(n):
+            expr = "h({0})".format(expr)
+        return eval(expr, {'h': h, 'x': x})
+    return inner
+
+    # def func(x):
+    #     nonlocal n
+    #     if 0 == n:
+    #         return x
+    #     if 1 == n:
+    #         return h(x)
+    #     else:
+    #         n -= 1
+    #         return h(func(x))
+    # return func
+
+def protected_secret(password, secret, num_attempts):
+    """
+    Returns a function which takes in a password and prints the SECRET if the password entered matches
+    the PASSWORD given to protected_secret. Otherwise it prints "INCORRECT PASSWORD". After NUM_ATTEMPTS
+    incorrect passwords are entered, the secret is locked and the function should print "SECRET LOCKED".
+
+    >>> my_secret = protected_secret("correcthorsebatterystaple", "I love UCB", 2)
+    >>> my_secret = my_secret("hax0r_1") # 2 attempts left
+    INCORRECT PASSWORD
+    >>> my_secret = my_secret("correcthorsebatterystaple")
+    I love UCB
+    >>> my_secret = my_secret("hax0r_2") # 1 attempt left
+    INCORRECT PASSWORD
+    >>> my_secret = my_secret("hax0r_3") # No attempts left
+    SECRET LOCKED
+    >>> my_secret = my_secret("correcthorsebatterystaple")
+    SECRET LOCKED
+    """
+    def get_secret(password_attempt):
+        nonlocal num_attempts
+        if num_attempts > 0:
+            if password == password_attempt:
+                print(secret)
+                return get_secret
+            else:
+                num_attempts -= 1
+                print("INCORRECT PASSWORD")
+                return get_secret
+        else:
+            print("SECRET LOCKED")
+            return get_secret
+            
+    return get_secret
+
+
+##########################
+# Just for fun Questions #
+##########################
+
+def zero(f):
+    return lambda x: x
+
+def successor(n):
+    return lambda f: lambda x: f(n(f)(x))
+
+def one(f):
+    """Church numeral 1: same as successor(zero)"""
+    return lambda x: f(zero(f)(x))
+
+def two(f):
+    """Church numeral 2: same as successor(successor(zero))"""
+    return lambda x: f(one(f)(x))
+
+three = successor(two)
+
+def church_to_int(n):
+    """Convert the Church numeral n to a Python integer.
+
+    >>> church_to_int(zero)
+    0
+    >>> church_to_int(one)
+    1
+    >>> church_to_int(two)
+    2
+    >>> church_to_int(three)
+    3
+    """
+    f = lambda x: x + 1
+    return n(f)(0)
+
+def add_church(m, n):
+    """Return the Church numeral for m + n, for Church numerals m and n.
+
+    >>> church_to_int(add_church(two, three))
+    5
+    """
+    return lambda f: lambda x: n(f)(m(f)(x))
+
+def mul_church(m, n):
+    """Return the Church numeral for m * n, for Church numerals m and n.
+    >>> four = successor(three)
+    >>> church_to_int(mul_church(two, three))
+    6
+    >>> church_to_int(mul_church(three, four))
+    12
+    """
+    return lambda f: lambda x: n(m(f))(x)
+
+def pow_church(m, n):
+    """Return the Church numeral m ** n, for Church numerals m and n.
+
+    >>> church_to_int(pow_church(two, three))
+    8
+    >>> church_to_int(pow_church(three, two))
+    9
+    """
+    return n(m)
